3100de755607b6c6b8fc1b9d1e58279d9c17642c
Naeem Akram <153123217+NaeemAbdullahAkram@users.noreply.github.com>
2025-07-16 08:10:49 +0500

    Time: 13 ms (42.86%), Space: 73.1 MB (28.57%) - LeetHub

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.js
@@ -0,0 +1,25 @@
+var maximumLength = function(nums) {
+    const N = nums.length;
+    
+    let evenOdd = new Array(N+1).fill(0);
+    let oddEven = new Array(N+1).fill(0);
+    let evenCount = +( (nums[N-1]%2) === 0);
+    let oddCount = +( (nums[N-1]%2) === 1);
+    
+    for(let I = N-2; I>=0; I--){
+        if(nums[I]%2){ oddCount++; }
+        else{ evenCount++; }
+        if( (nums[I]%2 == 0) && (nums[I+1]%2 == 1) ){
+            evenOdd[I] = Math.max( oddEven[I+1]+1,  2)
+        }else{
+            evenOdd[I] = evenOdd[I+1];
+        }
+        //Odd-Even... alternating
+        if( (nums[I]%2 == 1) && (nums[I+1]%2 == 0) ){
+            oddEven[I] = Math.max( evenOdd[I+1]+1,  2)
+        }else{
+            oddEven[I] = oddEven[I+1];
+        }
+    }
+    return Math.max(evenCount, oddCount, evenOdd[0], oddEven[0]);
+};
