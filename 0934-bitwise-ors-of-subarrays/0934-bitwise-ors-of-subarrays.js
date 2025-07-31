b916fe054406e37882a8c2aedf1dfbc130c728a8
Naeem Akram <153123217+NaeemAbdullahAkram@users.noreply.github.com>
2025-07-31 12:26:54 +0500

    Time: 205 ms (66.67%), Space: 84.8 MB (33.33%) - LeetHub

---
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.js
@@ -0,0 +1,12 @@
+var subarrayBitwiseORs = function (a) {
+    let uniqueBitwiseOrs = new Set();
+    let prevBitwiseOrs = new Set();
+    for (const e of a) {
+        const currOrs = new Set();
+        for (const por of prevBitwiseOrs) currOrs.add(por | e);
+        currOrs.add(e);
+        for (let e of currOrs) uniqueBitwiseOrs.add(e);
+        prevBitwiseOrs = currOrs;
+    }
+    return uniqueBitwiseOrs.size;
+};
